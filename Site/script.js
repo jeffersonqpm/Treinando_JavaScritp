2bb625d328bf9ed51d4494c5ee25ca74d4559111
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-31 19:36:24 -0300

    update

--- Site/script.js
+++ Site/script.js
@@ -1,36 +1,82 @@
 const botaoPlayPause = document.getElementById('play-pause');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const titulo = document.getElementById('capitulo');
+
 const audio = document.getElementById('audio-capitulo');
-let tocando = 0;
 
+let tocando = false;
+let capituloAtual = 1;
+const numeroCapitulo = 10;
 
-function tocarAudio() {
+function play() {
 
     audio.play();
-    botaoPlayPause.classList.remove('bi-play-circle-fill');
-    botaoPlayPause.classList.add('bi-pause-circle-fill');
-
+    botaoPlayPause.classList.remove('bi-play-circle');
+    botaoPlayPause.classList.add('bi-pause-circle')
+    tocando = true;
 }
 
-function pausarAudio() {
-
+function pause() {
     audio.pause();
-    botaoPlayPause.classList.remove('bi-pause-circle-fill');
-    botaoPlayPause.classList.add('bi-play-circle-fill');
+    botaoPlayPause.classList.remove('bi-pause-circle');
+    botaoPlayPause.classList.add('bi-play-circle')
+    tocando = false;
 
 }
 
-function PlayOrPause() {
+function playOrPause() {
 
-    if (tocando === 0) {
-
-        tocarAudio();
-        tocando = 1;
-
+    if (tocando == false) {
+        play();
+        tocando = true;
     } else {
-        pausarAudio();
-        tocando = 0;
+        pause();
+        tocando = false;
     }
 }
 
+function proximaFaixa() {
 
-botaoPlayPause.addEventListener('click', PlayOrPause);
+    if (capituloAtual === numeroCapitulo) {
+        capituloAtual = 1;
+
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audio.src = "/Site/books/dom-casmurro/" + capituloAtual + ".mp3";
+    play();
+    mudarTitulo();
+   
+}
+
+function voltarFaixa(){
+
+    if(capituloAtual === 1){
+
+        capituloAtual = numeroCapitulo;
+
+    }else{
+        capituloAtual--;
+    }
+
+    audio.src = "/Site/books/dom-casmurro/" + capituloAtual + ".mp3";
+    play();
+    mudarTitulo();
+   
+
+}
+
+function mudarTitulo(){
+
+    titulo.innerText = "Capítulo " + capituloAtual;
+}
+
+
+
+
+
+botaoPlayPause.addEventListener('click', playOrPause);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
